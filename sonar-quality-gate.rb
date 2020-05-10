f2041335280ebce4f026115b7b26305d1be30e11
Damien Cuenot <11711525+dcuenot@users.noreply.github.com>
2020-05-10 15:37:16 +0200

    Update sonar-quality-gate.rb

--- sonar-quality-gate.rb
+++ sonar-quality-gate.rb
@@ -3,7 +3,7 @@ class SonarQualityGate < Formula
   homepage "https://github.com/dcuenot/sonar-qg-in-cli"
   url "https://github.com/dcuenot/sonar-qg-in-cli/releases/download/v0.1.7/sonar-quality-gate-darwin-x86_64"
   version "0.1.17"
-  sha256 "9cbb157850eb779d43d06a8912b67d2e3126016d1fc80fbe3f56a60cc664e964"
+  sha256 "cac5179d8465b7554ce7e3763f28e611c7f1ecb1d00acea17f4fb4f65e19e479"
 
   def install
     bin.install "sonar-quality-gate"
